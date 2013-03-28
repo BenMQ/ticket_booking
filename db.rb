c555b6b98c6f1b077a279c3ede06e8b528629aee
Minqi Chen <cnchenminqi@gmail.com>
2013-03-28 20:33:21 +0800

    open sales for two new shows

--- db.rb
+++ db.rb
@@ -12,6 +12,8 @@ class Booking
   property :ticket1,      Integer, :default => 0
   property :ticket2,      Integer, :default => 0
   property :ticket3,      Integer, :default => 0
+  property :ticket4,      Integer, :default => 0
+  property :ticket5,      Integer, :default => 0
   property :payment_method, String
   property :note,         Text
   property :paid,         Boolean, :default => false
